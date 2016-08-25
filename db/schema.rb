4a9980af6b1ce426c3625103d933ce4d08bada9f
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2016-08-25 18:26:23 +0530

    Added association between user and job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825112530) do
+ActiveRecord::Schema.define(version: 20160825124704) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160825112530) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "company"
+    t.integer  "user_id"
   end
 
   create_table "searches", force: :cascade do |t|
